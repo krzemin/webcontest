7171312945155d165f584a17d638484560d74b7f
Piotr Krzemiński <pio.krzeminski@gmail.com>
2012-12-19 19:09:37 +0100

    codemirror change event handling

--- www-client/release/toaster/gui.js
+++ www-client/release/toaster/gui.js
@@ -4,15 +4,39 @@ var Gui,
 Gui = (function() {
 
   function Gui() {
+    this.codeChanged = __bind(this.codeChanged, this);
+
+    this.initCodeMirror = __bind(this.initCodeMirror, this);
+
     this.start = __bind(this.start, this);
 
   }
 
   Gui.prototype.start = function() {
-    return $('#code').codemirror({
-      value: 'int add(int a, int b) { return a + b; }',
-      mode: 'clike'
+    return this.initCodeMirror();
+  };
+
+  Gui.prototype.initCodeMirror = function() {
+    var cm, codeWidget, opts,
+      _this = this;
+    opts = {
+      mode: "text/x-c++src",
+      theme: "eclipse",
+      lineNumbers: true
+    };
+    codeWidget = document.getElementById('code');
+    cm = CodeMirror.fromTextArea(codeWidget, opts);
+    return CodeMirror.on(cm, "change", function(instance, chObj) {
+      return _this.codeChanged(instance, chObj);
     });
+  };
+
+  Gui.prototype.codeChanged = function(instance, chObj) {
+    console.log('from: ' + chObj.from);
+    console.log('to: ' + chObj.to);
+    console.log('text: ' + chObj.text);
+    console.log('next: ' + chObj.next);
+    return console.log('TXT: ' + instance.getValue());
   };
 
   return Gui;
--- www-client/src/gui.coffee
+++ www-client/src/gui.coffee
@@ -2,8 +2,24 @@ class Gui
   constructor: ->
 
   start: =>
-    $('#code').codemirror({
-                value: 'int add(int a, int b) { return a + b; }',
-                mode: 'clike'
-            })
+    @initCodeMirror()
+
+  initCodeMirror: =>
+    opts = {
+      mode: "text/x-c++src",
+      theme: "eclipse",
+      lineNumbers: true,
+    }
+    codeWidget = document.getElementById('code')
+    cm = CodeMirror.fromTextArea(codeWidget, opts)
+    CodeMirror.on(cm, "change", (instance, chObj) => @codeChanged(instance, chObj))
+
+  codeChanged: (instance, chObj) =>
+    console.log('from: ' + chObj.from)
+    console.log('to: ' + chObj.to)
+    console.log('text: ' + chObj.text)
+    console.log('next: ' + chObj.next)
+    console.log('TXT: ' + instance.getValue())
+
+
 
