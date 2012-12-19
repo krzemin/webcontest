7fdb9e2d052e1ea040487b04e91bc8bab3bd9b0c
Piotr Krzemiński <pio.krzeminski@gmail.com>
2012-12-19 15:45:37 +0100

    initial toaster project with handlebars templates compiling

---
+++ www-client/release/toaster/app.js
@@ -0,0 +1,20 @@
+var App;
+
+App = (function() {
+
+  function App() {
+    var glue, gui, localStorage, useCase;
+    useCase = new UseCase();
+    gui = new Gui();
+    localStorage = new LocalStorage("app");
+    glue = new Glue(useCase, gui, localStorage);
+    useCase.start();
+  }
+
+  return App;
+
+})();
+
+$(function() {
+  return new App();
+});
