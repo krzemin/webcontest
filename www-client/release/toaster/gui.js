aee30d6952603f0b31f2cfb778970772f40091fe
Piotr Krzemiński <pio.krzeminski@gmail.com>
2012-12-20 21:44:47 +0100

    trying to get codemirror editor on fullscreen (not working yet)

--- www-client/release/toaster/gui.js
+++ www-client/release/toaster/gui.js
@@ -8,6 +8,10 @@ Gui = (function() {
 
     this.saveCodeCallback = __bind(this.saveCodeCallback, this);
 
+    this.setCodeViewFullscreen = __bind(this.setCodeViewFullscreen, this);
+
+    this.isCodeViewFullScreen = __bind(this.isCodeViewFullScreen, this);
+
     this.initCodeView = __bind(this.initCodeView, this);
 
     this.start = __bind(this.start, this);
@@ -17,18 +21,49 @@ Gui = (function() {
   Gui.prototype.start = function() {};
 
   Gui.prototype.initCodeView = function(codeText) {
-    var codeWidget, opts;
+    var codeWidget, opts,
+      _this = this;
     opts = {
       mode: "text/x-c++src",
       theme: "monokai",
       lineNumbers: true,
-      indentUnit: 4
+      indentUnit: 4,
+      extraKeys: {
+        "F11": function(cm) {
+          return _this.setCodeViewFullScreen(cm, !_this.isCodeViewFullScreen(cm));
+        },
+        "Esc": function(cm) {
+          if (_this.isCodeViewFullScreen(cm)) {
+            return _this.setCodeViewFullScreen(cm, false);
+          }
+        }
+      }
     };
     codeWidget = document.getElementById('code');
     this.cm = CodeMirror.fromTextArea(codeWidget, opts);
     this.cm.setValue(codeText);
     this.cm.markClean();
     return setTimeout(this.saveCodeCallback, this.saveCodeEvery * 1000);
+  };
+
+  Gui.prototype.isCodeViewFullScreen = function(cm) {
+    return /\bCodeMirror-fullscreen\b/.test(cm.getWrapperElement().className);
+  };
+
+  Gui.prototype.setCodeViewFullscreen = function(cm, full) {
+    var winHeight, wrap;
+    wrap = cm.getWrapperElement();
+    winHeight = window.innerHeight || (document.documentElement || document.body).clientHeight;
+    if (full) {
+      wrap.className += " CodeMirror-fullscreen";
+      wrap.style.height = winHeight + "px";
+      document.documentElement.style.overflow = "hidden";
+    } else {
+      wrap.className = wrap.className.replace(" CodeMirror-fullscreen", "");
+      wrap.style.height = "";
+      document.documentElement.style.overflow = "";
+    }
+    return cm.refresh;
   };
 
   Gui.prototype.saveCodeCallback = function() {
--- www-client/src/gui.coffee
+++ www-client/src/gui.coffee
@@ -9,7 +9,11 @@ class Gui
       mode: "text/x-c++src",
       theme: "monokai",
       lineNumbers: true,
-      indentUnit: 4 
+      indentUnit: 4,
+      extraKeys: {
+        "F11": (cm) => @setCodeViewFullScreen(cm, !@isCodeViewFullScreen(cm))
+        "Esc": (cm) => @setCodeViewFullScreen(cm, false) if @isCodeViewFullScreen(cm)
+      }
     }
     codeWidget = document.getElementById('code')
     @cm = CodeMirror.fromTextArea(codeWidget, opts)
@@ -18,6 +22,22 @@ class Gui
     setTimeout(@saveCodeCallback, @saveCodeEvery * 1000)
     # previously, saving the code was performed every editor's content change
     # CodeMirror.on(@cm, "change", (instance, chObj) => @codeChanged(instance.getValue()))
+
+  isCodeViewFullScreen: (cm) =>
+    /\bCodeMirror-fullscreen\b/.test(cm.getWrapperElement().className)
+
+  setCodeViewFullscreen: (cm, full) =>
+    wrap = cm.getWrapperElement()
+    winHeight = window.innerHeight || (document.documentElement || document.body).clientHeight
+    if full
+      wrap.className += " CodeMirror-fullscreen"
+      wrap.style.height = winHeight + "px"
+      document.documentElement.style.overflow = "hidden"
+    else
+      wrap.className = wrap.className.replace(" CodeMirror-fullscreen", "")
+      wrap.style.height = ""
+      document.documentElement.style.overflow = ""
+    cm.refresh
 
   saveCodeCallback: =>
     @codeChanged(@cm.getValue()) unless @cm.isClean()
